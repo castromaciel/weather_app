c4c52354fa0ac114fb6c969f04d8d39189a7f3d4
castromaciel <castromaciel00@gmail.com>
2022-08-21 20:16:12 -0300

    fix cellphone, and tablet view

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { useHightlightsContext } from '../../Provider/HightlightsProvider';
 import CardHighlights from '../CardHighlights/CardHighlights';
 
-const Highlights = () => {
+const Highlights = ({ className }) => {
   const hightlights = useHightlightsContext();
 
   return (
-    <div className='mx-5 px-5'>
-      <h3 className='mt-3'>Today&apos;s Hightlights</h3>
-      <section className='row justify-content-between'>
+    <div className={`${className}`}>
+      <h3 className='ms-md-4 ms-lg-0 mt-3'>Today&apos;s Hightlights</h3>
+      <section className='row justify-content-center justify-content-md-evenly justify-content-lg-between'>
         {
           hightlights?.map(hightlight => (
             <CardHighlights
-              className={'col-5 text-white bg-darkblue-75'}
+              className={'col-9 col-md-5 col-lg-5 text-white bg-darkblue-75'}
               hightlight={hightlight}
               key={hightlight.title}
             />
